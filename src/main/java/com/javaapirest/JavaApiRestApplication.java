8f7c09c07f84d39e727775f11271a172670824d2
DESKTOP-OQKEK4I\Computador <lucasmpbarga@gmail.com>
2018-10-04 22:16:29 -0300

    start

---
+++ src/main/java/com/javaapirest/JavaApiRestApplication.java
@@ -0,0 +1,12 @@
+package com.javaapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javaapirest/JavaApiRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaapirest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaApiRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
