245d496974cb0778b6a708dc6305395e07d864b9
DESKTOP-OQKEK4I\Computador <lucasmpbarga@gmail.com>
2018-10-06 17:19:43 -0300

    api okay

--- src/main/java/com/javaapirest/domain/Produto.java
+++ src/main/java/com/javaapirest/domain/Produto.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer produto_id;
