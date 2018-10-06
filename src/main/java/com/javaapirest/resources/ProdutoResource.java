f1b9aeb6dd303557eeda4e6d0991e53723f0d7f1
DESKTOP-OQKEK4I\Computador <lucasmpbarga@gmail.com>
2018-10-06 12:43:20 -0300

    rest funcionando.

--- src/main/java/com/javaapirest/resources/ProdutoResource.java
+++ src/main/java/com/javaapirest/resources/ProdutoResource.java
@@ -1,29 +1,28 @@
 package com.javaapirest.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javaapirest.services.ProdutoService;
+import com.javaapirest.domain.Produto;
 
 @RestController
 @RequestMapping(value="/produtos")
 public class ProdutoResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Produto> listar() {
+		Produto p1 = new Produto(1, "123","Informatica","Descricao",2,3);
+		Produto p2 = new Produto(2, "123","Escritorio","Descricao",3,4);
 		
-		return "REST okay.";
+		List<Produto> lp = new ArrayList<>();
+		
+		lp.add(p1);
+		lp.add(p2);
+			
+		return lp;
 	}
-	
-	@Autowired
-	private ProdutoService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Object obj = service.buscar(id);		
-		return ResponseEntity.ok().body(obj);
-	}
+
 }
